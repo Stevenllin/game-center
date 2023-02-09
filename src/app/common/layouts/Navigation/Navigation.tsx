d0e423c22e9407cd90cc92aaa4c6cca2b8a1be29
Steven <stevenlin3263@gmail.com>
2023-02-09 12:15:28 +0800

    [feat] add the animation to navigation section

--- src/app/common/layouts/Navigation/Navigation.tsx
+++ src/app/common/layouts/Navigation/Navigation.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { RootState } from 'app/store/types';
 import { HiCreditCard } from "react-icons/hi2";
 import { FaRegCircle } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
 import { IoMan } from "react-icons/io5";
+import { motion } from "framer-motion";
+import { NavigationStateValuesEnum } from 'app/core/enum/element/navigationStateValuesEnum';
+import { setGlobalNavigationState } from 'app/store/global/action';
 
 const Navigation: React.FC = () => {
+  const navigationState = useSelector((state: RootState) => state.global.navigationState);
+  const reduxDispatch = useDispatch();
+  /**
+   * @description 
+  */
+   const handleSetNavigationState = (value: NavigationStateValuesEnum) => {
+    reduxDispatch(setGlobalNavigationState(value));
+  }
   return (
     <nav>
       <div className="h-100 d-flex flex-column align-items-center justify-content-center">
         <div className="my-4">
-          <HiCreditCard />
+          <motion.button
+            type="button"
+            className={navigationState === NavigationStateValuesEnum.Quiz ? 'nav-active' : ''}
+            onClick={() => handleSetNavigationState(NavigationStateValuesEnum.Quiz)}
+            whileHover={{ scale: 1.4, rotate: 360 }}
+          >
+            <HiCreditCard />
+          </motion.button>
         </div>
         <div className="my-4">
-          <FaRegCircle />
+          <motion.button
+            type="button"
+            className={navigationState === NavigationStateValuesEnum.OXO ? 'nav-active' : ''}
+            onClick={() => handleSetNavigationState(NavigationStateValuesEnum.OXO)}
+            whileHover={{ scale: 1.4, rotate: 360 }}
+          >
+            <FaRegCircle />
+          </motion.button>
         </div>
         <div className="my-4">
-          <BsGridFill />
+          <motion.button
+            type="button"
+            className={navigationState === NavigationStateValuesEnum.TWO ? 'nav-active' : ''}
+            onClick={() => handleSetNavigationState(NavigationStateValuesEnum.TWO)}
+            whileHover={{ scale: 1.4, rotate: 360 }}
+          >
+            <BsGridFill />
+          </motion.button>
         </div>
         <div className="my-4">
-          <IoMan />
+          <motion.button
+            type="button"
+            className={navigationState === NavigationStateValuesEnum.Hang ? 'nav-active' : ''}
+            onClick={() => handleSetNavigationState(NavigationStateValuesEnum.Hang)}
+            whileHover={{ scale: 1.4, rotate: 360 }}
+          >
+            <IoMan />
+          </motion.button>
         </div>
       </div>
     </nav>
