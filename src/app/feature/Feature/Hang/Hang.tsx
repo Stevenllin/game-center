f4b63759dcc82a71d6ff2715ffb18a8710eba620
Steven <stevenlin3263@gmail.com>
2023-02-13 09:40:45 +0800

    [feat] build the keypress event

--- src/app/feature/Feature/Hang/Hang.tsx
+++ src/app/feature/Feature/Hang/Hang.tsx
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import words from './wordList.json';
 import { Keyboard } from './data';
 
+const HEAD = (
+  <div key="head" className="hang-head" />
+)
+
+const BODY = (
+  <div key="body" className="hang-body" />
+)
+
+const RIGHT_ARM = (
+  <div key="rarm" className="hang-right-arm" />
+)
+
+const LEFT_ARM = (
+  <div key="larm" className="hang-left-arm" />
+)
+
+const RIGHT_LEG = (
+  <div key="rleg" className="hang-right-leg" />
+)
+
+const LEFT_LEG = (
+  <div key="lleg" className="hang-left-leg" />
+)
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, LEFT_LEG, RIGHT_LEG]
+
 const Hang: React.FC = () => {
-  const word = 'test';
+  const handleGetRandomWord = () => {
+    return words[Math.floor(Math.random() * words.length)]
+  }
+  const [wordToGuess, setWordToGuess] = useState<string>('test');
+  const [guessedWord, setGuessedWord] = useState<string[]>([]);
+  const incorrectLetter = guessedWord.filter(letter => !wordToGuess.includes(letter));
+
+  console.log('wordToGuess', wordToGuess);
+
+  const handleAddGuessedLetter = (letter: string) => {
+    if (guessedWord.includes(letter)) return
+
+    setGuessedWord(currentLetter => [...currentLetter, letter]);
+  }
+
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return
+      e.preventDefault()
+      handleAddGuessedLetter(key)
+    }
+
+    document.addEventListener('keypress', handler)
+
+    return () => {
+      document.removeEventListener('keypress', handler)
+    }
+  }, [guessedWord])
 
   return (
     <div id="hang" className="hang-container">
       {/** hangman section */}
       <div className="hang-wrapper">
-        <div className="hang-head" />
-        <div className="hang-body" />
-        <div className="hang-right-arm" />
-        <div className="hang-left-arm" />
-        <div className="hang-right-leg" />
-        <div className="hang-left-leg" />
-
+        {BODY_PARTS.slice(0, incorrectLetter.length)}
         <div className="hang-device1" />
         <div className="hang-device2" />
         <div className="hang-device3" />
@@ -23,8 +73,16 @@ const Hang: React.FC = () => {
 
       {/** hangman word */}
       <div className="hang-word">
-        {word.split('').map((letter, index) => (
-          <span key={index} className="hang-word-item">{letter}</span>
+        {wordToGuess.split('').map((letter, index) => (
+          <span key={index} className="hang-word-item d-flex justify-content-center">
+            <span
+              style={{
+                visibility: guessedWord.includes(letter) ? 'visible' : 'hidden'
+              }}
+            >
+              {letter}
+            </span>
+          </span>
         ))}
       </div>
 
